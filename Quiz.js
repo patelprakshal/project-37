784265f3d80ed91a2f767f7fdedcdd0fde95f7ed
patelprakshal <74961658+patelprakshal@users.noreply.github.com>
2021-06-10 11:13:08 +0530

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,72 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    //write code here to hide question elements
+
+    //write code to change the background color here
+
+    //write code to show a heading for showing the result of Quiz
+
+    //call getContestantInfo( ) here
+    function play(){
+      if(allContestants !== undefined){
+      fill("Blue");
+      textSize(20);
+      text("*NOTE: contestant who answered correct are highlighted in green color!",130,230);
+      }
+      for(var plr in allContestants){
+      var correctAns = "2"
+      if(correctAns === allContestants[plr].answer){
+        fill("Green")
+      } else
+         fill("red")
+        
+        
+    
+       
+       
+     
+      }
+    
+     
+    }
+
+
+    //write condition to check if contestantInfor is not undefined
+
+    //write code to add a note here
+
+    //write code to highlight contest who answered correctly
+
+    
+  }
+
+}
